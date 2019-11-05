c2f3deb57febcdf2f8a51f01f813091736c8573e
Frank Ruiz <frank.ruiz@comcast.net>
2019-11-04 21:21:54 -0800

    changed the first system out message

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ public class Hello
 	public static void main(String argv[])
 	{
 		for (int j = 0 ; ji < 10; j ++) {
-			System.out.println("Brendan is making a change");
+			System.out.println("go to github.com");
 		}
 		System.out.println("Hello, World");
 	}
